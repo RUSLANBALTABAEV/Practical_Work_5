f422edab7b96fa319bc8fc5da7b35fb89772d910
RUSLANBALTABAEV <63105241+RUSLANBALTABAEV@users.noreply.github.com>
2022-04-12 15:31:43 +0600

    Creating Open Source Code by Practical Work 5

---
+++ Practical_work_5_Task_2_Listing_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double S()
+{
+  double a, b, c, g, h, S1, S2, S3;
+  S1 = 0.5 * c * c * sin(a) * cos(a);
+  S2 = 0.5 * a * a * tan(b);
+  S3 = 0.5 * h * h / (sin(g) * cos(g));
+}
+
+int main()
+{
+  setlocale(LC_ALL, "Russian");
+  double a, b, c, g, h, S1, S2, S3;
+  cout << ""; cin >> ;
+  if () cout << "Площадь первого прямоугольного треугольника больше! ";
+  if () cout << "Площадь второго прямоугольного треугольника больше! ";
+  if () cout << "Площадь третьего прямоугольного треугольника больше! ";
+  return 0;
+}
---
+++ Practical_work_5_Task_2_Listing_3.cpp
@@ -0,0 +1,36 @@
+﻿// Practical_work_5_Task_2_Listing_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>  // Заголовочный файл с классами, функциями и пеменными для организации ввода-вывода
+#include <conio.h>   // Заголовочный файл, консольный ввод-вывод
+#include <math.h>    // Заголовочный файл, для использование мат-функций
+#include <iomanip>	 // Заголовок <iomanip> объекта iostreams для определения нужного числа манипуляторов, каждый из которых принимает один аргумент
+
+using namespace std; // использование пространство имён
+
+long long  factorial(long long  n)
+{
+    return n <= 1 ? 1 : n * factorial(n - 1);
+}
+
+double  get_P(long long  n)
+{
+    return  pow(3.0, double(n)) * exp(-3.0) / factorial(n);
+}
+
+int main()
+{
+    std::locale::global(std::locale(""));
+    const long long  N_MIN = 0;
+    const long long  N_MAX = 20;
+    for (;;)
+    {
+        long long  n = 0;
+
+        cout << "В порт приходят в среднем 3 корабля в день." << endl << "Введите количество кораблей для вычисления вероятности " << N_MIN << ".." << N_MAX << ": "; cin >> n;
+        if (n < N_MIN || N_MAX < n) break;
+        cout << "Вероятность того, что в порт в день придут " << n << " кораблей, равна: " << fixed << setprecision(10) << get_P(n) << "." << endl << endl << endl << endl;
+    }
+    _getch();
+    return 0;
+}

78c81de4262da2099d7a6a737b127da843a1da25
RUSLANBALTABAEV <63105241+RUSLANBALTABAEV@users.noreply.github.com>
2022-04-18 17:27:13 +0600

    Adding Practical_work_5_Task_2_Listing_1.cpp

---
+++ Practical_work_5_Task_2_Listing_1.cpp
@@ -0,0 +1,43 @@
+﻿// Practical_work_5_Task_2_Listing_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+long double S(double a1, double b1, double a2, double b2, double a3, double b3)
+{
+	double S1, S2, S3;
+	
+	S1 = (a1 * b1) / 2;
+	S2 = (a2 * b2) / 2;
+	S3 = (a3 * b3) / 2;
+	
+	if (S1 >= S2 && S1 >= S3)
+	{
+		cout << "Площадь первого треугольника больше! " << S1 << endl;
+	}
+
+	if (S2 >= S1 && S2 >= S3)
+	{
+		cout << "Площадь второго треугольника больше! " << S2 << endl;
+	}
+
+	if (S3 >= S2 && S3 >= S1)
+	{
+		cout << "Площадь третьего треугольника больше! " << S3 << endl;
+	}
+	return S1, S2, S3;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x1, y1, x2, y2, z1, z2;
+	cout << "Введите катеты прямоуольных треугольников через пробел значение a1, b1, a2, b2, a3, b3: ";
+	cin >> x1 >> y1 >> x2 >> y2 >> z1 >> z2;
+	S(x1, y1, x2, y2, z1, z2);
+	_getch();
+	return 0;
+}
